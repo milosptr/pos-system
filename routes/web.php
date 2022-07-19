b573dc9000396cbccbebb92399270182595a6775
milospetrovic <milosp@ultimatemovers.net>
2022-07-20 01:29:08 +0200

    add middleware for administrator

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware' => ['administrator', 'auth']], function () {
+  Route::get('/dashboard', function () {
+      return view('dashboard');
+  })->name('dashboard');
+});
 
 Route::get('/app', function () {
   return view('app');
-});
+})->name('app');
 
 require __DIR__.'/auth.php';
