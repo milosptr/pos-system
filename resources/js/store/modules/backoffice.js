1dc64c14e779b176e90410dafdd76b8538e4e173
mrpannic <nikola@ultimatemovers.net>
2022-07-25 19:05:19 +0200

    backoffice state - tables/orders/invoices

--- resources/js/store/modules/backoffice.js
+++ resources/js/store/modules/backoffice.js
@@ -7,6 +7,9 @@ const backoffice = {
         categories: [],
         inventory: [],
         users: [],
+        tables: [],
+        orders: [],
+        invoices: [],
         ordersFilters: {
             date: null
         }
