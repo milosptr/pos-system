12c07818d98406302660199949768dc8644bcd38
milospetrovic <milosp@ultimatemovers.net>
2022-07-30 21:03:13 +0200

    add inventory total price at the bottom of table

--- resources/js/components/Backoffice/BackofficeInventory.vue
+++ resources/js/components/Backoffice/BackofficeInventory.vue
@@ -44,6 +44,16 @@
                   :class="{'bg-gray-50': idx % 2 === 1 }"
                 />
               </tbody>
+              <tr>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 py-3 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">Total</th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"></th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell text-center">{{ $filters.formatPrice(inventoryItemsPrice) }} RSD</th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter whitespace-nowrap"></th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"></th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"></th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"></th>
+                <th scope="col" class="sticky top-0 z-10 border-b border-t border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"></th>
+              </tr>
             </table>
           </div>
         </div>
@@ -72,6 +82,9 @@ export default {
       },
       categories() {
         return this.$store.getters.categories
+      },
+      inventoryItemsPrice() {
+        return this.$store.getters.inventory.reduce((a, v) => a + v.price, 0)
       },
     },
     mounted() {
