63432fbaa1aa67cf1a369e084da6a26dc8b9ed08
milospetrovic <milosp@ultimatemovers.net>
2022-08-03 20:51:43 +0200

    request fullscreen app

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,6 +24,17 @@ app.config.globalProperties.$filters = {
   }
 }
 
-alert(window.innerWidth + ' x ' + window.innerHeight)
+function enterFullScreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  }else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();     // Firefox
+  }else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();  // Safari
+  }else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();      // IE/Edge
+  }
+}
+enterFullScreen()
 
 app.mount('#app')
