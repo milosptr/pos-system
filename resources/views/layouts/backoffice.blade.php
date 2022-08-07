52d7e4c1459f314ca692a7899c5227b43fe3114a
milospetrovic <milosp@ultimatemovers.net>
2022-08-07 13:07:41 +0200

    make backoffice pwa

--- resources/views/layouts/backoffice.blade.php
+++ resources/views/layouts/backoffice.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
     <head>
         <meta charset="utf-8">
+        <link rel="manifest" href="backoffice.webmanifest">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
