38c0fece918b881f69e5332633da3e5eda23e3e8
milospetrovic <milosp@ultimatemovers.net>
2022-08-20 11:48:21 +0200

    pagination responsive

--- resources/js/components/Backoffice/BackofficeInvoicesPagination.vue
+++ resources/js/components/Backoffice/BackofficeInvoicesPagination.vue
@@ -28,6 +28,7 @@
       <div
         v-if="pagination.links.next"
         class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 cursor-pointer"
+        :class="{'ml-auto': !pagination.links.prev}"
         @click="nextPage"
       >
         Next
