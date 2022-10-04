cec925d97c2927ccd22899f58f1d39c6aee7c566
milospetrovic <milosp@ultimatemovers.net>
2022-10-04 22:36:50 +0000

    allow origin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,6 +13,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
