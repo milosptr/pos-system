89f6208eb9a85bbf5bf4e722359422f0b2308122
milospetrovic <milosp@ultimatemovers.net>
2022-11-12 17:05:39 +0000

    add sentry

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import vClickOutside from "click-outside-vue3"
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Sentry from "@sentry/vue"
+import { BrowserTracing } from "@sentry/tracing"
 
 const app = createApp(App)
   .use(router)
@@ -23,5 +25,17 @@ app.config.globalProperties.$filters = {
     return new URL('/images/' + url, import.meta.env.VITE_APP_URL).href
   }
 }
+
+Sentry.init({
+  app,
+  dsn: "https://7ab1563e9f7e402fb49a68005d993c6d@o4504147121274880.ingest.sentry.io/4504147122978816",
+  environment: 'localhost',
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+    }),
+  ],
+  tracesSampleRate: 1.0,
+})
 
 app.mount('#app')
