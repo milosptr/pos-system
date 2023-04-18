d45ce679623be874c7ef97ad38e2d1ff51007556
milospetrovic <milosp@ultimatemovers.net>
2023-04-18 23:49:40 +0000

    replace img with svg

--- resources/js/components/Backoffice/Stockroom/ImportStockroom.vue
+++ resources/js/components/Backoffice/Stockroom/ImportStockroom.vue
@@ -4,7 +4,7 @@
       <div for="magimport"
       @click="showModal = true"
       class="inline-block gap-3 px-3 py-2 cursor-pointer border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      <img src="/images/importpackage.svg" width="20" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height="20" width="20"><g><path d="M3.5,13.5h-2a1,1,0,0,1-1-1v-8h13v8a1,1,0,0,1-1,1h-2" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></path><polyline points="4.5 10 7 7.5 9.5 10" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></polyline><line x1="7" y1="7.5" x2="7" y2="13.5" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></line><path d="M11.29,1A1,1,0,0,0,10.45.5H3.55A1,1,0,0,0,2.71,1L.5,4.5h13Z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></path><line x1="7" y1="0.5" x2="7" y2="4.5" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"></line></g></svg>
     </div>
     </div>
     <div v-if="showModal" class="fixed left-0 top-0 z-[100] w-full h-screen flex justify-center items-center">
