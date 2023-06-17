a2a85b3df8aaec8cb4b35b322c37a41e6ace7d3c
milospetrovic <milosp@ultimatemovers.net>
2023-06-18 00:36:59 +0200

    remove v model number

--- resources/js/components/Backoffice/BankInvoices/AddBankInvoiceModal.vue
+++ resources/js/components/Backoffice/BankInvoices/AddBankInvoiceModal.vue
@@ -49,7 +49,7 @@
           <div>
             <label for="amount" class="block text-sm font-medium leading-6 text-gray-900">Iznos</label>
             <div class="mt-2">
-              <input type="text" v-model.number="invoice.amount" name="amount" id="amount" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input type="text" v-model="invoice.amount" name="amount" id="amount" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
           <div>
