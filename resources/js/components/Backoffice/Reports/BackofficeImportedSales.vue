a6d8fed6fdd52be493de184398dc1ac6028d7dd0
Milos <milos@sidekickhealth.com>
2023-08-13 23:07:07 +0000

    remove for date column from imported sales tabel

--- resources/js/components/Backoffice/Reports/BackofficeImportedSales.vue
+++ resources/js/components/Backoffice/Reports/BackofficeImportedSales.vue
@@ -10,13 +10,10 @@
               ID</th>
             <th scope="col"
               class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell">
-              Filename</th>
+              Fajl</th>
             <th scope="col"
               class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">
-              For date</th>
-            <th scope="col"
-              class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">
-              Import date</th>
+              Datum uvoza</th>
             <th scope="col"
               class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter">
             </th>
@@ -31,9 +28,6 @@
             <td
               :class="[idx !== imports.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap px-3 py-2 text-sm text-gray-500']">
               {{ item.filename }}</td>
-              <td
-              :class="[idx !== imports.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap px-3 py-2 text-sm text-gray-500']">
-              {{ item.sales && item.sales.length ? formatDate(item.sales[0].created_at) : '' }}</td>
               <td
               :class="[idx !== imports.length - 1 ? 'border-b border-gray-200' : '', 'whitespace-nowrap px-3 py-2 text-sm text-gray-500']">
               {{ formatDateTime(item.created_at) }}</td>
