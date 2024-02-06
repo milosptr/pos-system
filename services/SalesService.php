bc3e5f5eac0de8fe1354541f7ef48eb2da580f86
Milos Petrovic <milos@sidekickhealth.com>
2024-02-06 23:47:33 +0000

    linter errors

--- services/SalesService.php
+++ services/SalesService.php
@@ -13,8 +13,8 @@ class SalesService {
     * Calculates in percent, the change between 2 numbers.
     * e.g from 1000 to 500 = 50%
     *
-    * @param $orders The orders to be saved
-    * @param $invoice The invoice to be saved
+    * @param $orders - The orders to be saved
+    * @param $invoice - The invoice to be saved
     */
     public static function parseAndSaveOrder($orders, $invoice)
     {
