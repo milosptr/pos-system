1d4c697733b3d8bf3125ed0a44875b27538ec738
Milos Petrovic <milos@sidekickhealth.com>
2024-02-10 19:02:43 +0000

    move unit to column

--- resources/js/components/Backoffice/Warehouse/BackofficeWarehouseSales.vue
+++ resources/js/components/Backoffice/Warehouse/BackofficeWarehouseSales.vue
@@ -16,9 +16,10 @@
           @click="nextDate()" />
       </div>
       <div class="mt-6 border border-solid border-gray-200 bg-white">
-        <div class="grid grid-cols-5 font-semibold text-sm py-1 border-b border-solid border-gray-300 bg-gray-200">
+        <div class="grid grid-cols-6 font-semibold text-sm py-1 border-b border-solid border-gray-300 bg-gray-200">
           <!--          <div class="px-4">Datum</div>-->
           <div class="px-4">Sirovina</div>
+          <div class="px-4">Jedinica</div>
           <div class="px-4 text-center">Prethodno stanje</div>
           <div class="px-4 text-center">Prodato</div>
           <div class="px-4 text-center">Uneto</div>
@@ -27,10 +28,11 @@
         <div
           v-for="(item, index) in warehouse"
           :key="item.id"
-          class="grid grid-cols-5 text-sm py-1"
+          class="grid grid-cols-6 text-sm py-1"
           :class="{ 'bg-gray-100': index % 2 === 1 }">
           <!--          <div class="px-4">{{ dayjs(item.date).format('DD.MM.YYYY') }}</div>-->
-          <div class="px-4">{{ item.warehouse.name }} ({{ item.warehouse.unit }})</div>
+          <div class="px-4">{{ item.warehouse.name }}</div>
+          <div class="px-4">{{ item.warehouse.unit }}</div>
           <div class="px-4 text-center">{{ item.previous_quantity }}</div>
           <div class="px-4 text-red-600 font-medium text-center">{{ item.sale_quantity }}</div>
           <div class="px-4 text-green-600 font-medium text-center">{{ item.import_quantity }}</div>
